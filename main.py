fb6274e786ca4997cd3402ca82a4afdb898e58c9
OvAlexander <alexanderleeov@gmail.com>
2024-08-02 15:40:37 -0400

    chore: got rid of default example

--- main.py
+++ main.py
@@ -280,32 +280,6 @@ def gather_file_names(directory_path):
             file_names.append(file)
     return file_names
 
-    # --- examples -------
-sentences = ["VADER is smart, handsome, and funny.",  # positive sentence example
-             # punctuation emphasis handled correctly (sentiment intensity adjusted)
-             "VADER is smart, handsome, and funny!",
-             # booster words handled correctly (sentiment intensity adjusted)
-             "VADER is very smart, handsome, and funny.",
-             "VADER is VERY SMART, handsome, and FUNNY.",  # emphasis for ALLCAPS handled
-             # combination of signals - VADER appropriately adjusts intensity
-             "VADER is VERY SMART, handsome, and FUNNY!!!",
-             # booster words & punctuation make this close to ceiling for score
-             "VADER is VERY SMART, uber handsome, and FRIGGIN FUNNY!!!",
-             "VADER is not smart, handsome, nor funny.",  # negation sentence example
-             "The book was good.",  # positive sentence
-             # negated negative sentence with contraction
-             "At least it isn't a horrible book.",
-             # qualified positive sentence is handled correctly (intensity adjusted)
-             "The book was only kind of good.",
-             # mixed negation sentence
-             "The plot was good, but the characters are uncompelling and the dialog is not great.",
-             "Today SUX!",  # negative slang with capitalization emphasis
-             # mixed sentiment example with slang and constrastive conjunction "but"
-             "Today only kinda sux! But I'll get by, lol",
-             "Make sure you :) or :D today!",  # emoticons handled
-             "Catch utf-8 emoji such as such as 💘 and 💋 and 😁",  # emojis handled
-             "Not bad at all"  # Capitalized negation
-             ]
 
 custom = ["Today was awful due to my code not working properly and barely getting anything done.",
           "Jackie and I worked on the weekly report slide.",
