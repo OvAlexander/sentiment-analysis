66e4f03c95f1ab118482d9f5e0076be351350c6a
OvAlexander <alexanderleeov@gmail.com>
2024-08-13 18:14:44 -0700

    feat: added attendance parser and team maker

--- utilities.py
+++ utilities.py
@@ -1,4 +1,8 @@
+import sys
+import argparse
+import random
 import hashlib
+import csv
 from calc import scale
 # Generates random color based on string
 
@@ -21,7 +25,34 @@ def string_to_rgb(s):
     return (r, g, b)
 
 
+def get_names(file: str):
+    names = []
+    with open(file, newline='') as csvfile:
+        rows = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in rows:
+            last_name = row[0][1:len(row[0])]
+            first_name = (row[1][1:-1])
+            names.append((first_name, last_name))
+        names.pop(0)
+        names.pop(0)
+    return names
+
+
+def makeGroup(lines, group_size, group_num):
+    leftovers = len(lines) % group_size
+
+    group = random.sample(lines, group_size)
+    for x in group:
+        lines.remove(x)
+    print("Group ", group_num, ": ", group)
+    return lines, group_size, group_num
+
+
 if __name__ == '__main__':
     equals_test = "example"
     assert (string_to_rgb(equals_test) == string_to_rgb(equals_test))
     print(string_to_rgb(equals_test))
+
+    names = get_names(
+        "_BWSI 2024 UAS-SAR (Synthetic Aperture Radar) Attendance - Sheet1.csv")
+    print(names)
